007b9a5dd0cde57b8aede2b807d228316e16c57f
kingtarun1 <51032756+kingtarun1@users.noreply.github.com>
2019-07-18 15:44:11 +0530

    Uploaded New Plugin

---
+++ stdplugins/ok.py
@@ -0,0 +1,3 @@
+Please set PRIVATE_GROUP_BOT_API_ID for the proper functioning of afk functionality in @UniBorg
+
+ An invalid Peer was used. Make sure to pass the right peer type (caused by SendMessageRequest)
