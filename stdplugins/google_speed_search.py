c995815802d90f52344b5643fcff8866e3313df5
SnapDragon <52431319+SnapDragon7410@users.noreply.github.com>
2019-08-02 15:41:17 +0200

    Update google_speed_search.py

--- stdplugins/google_speed_search.py
+++ stdplugins/google_speed_search.py
@@ -17,10 +17,10 @@ async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    sample_url = "https://da.gd/s?url=https://http://www.google.com/search?q={}&btnG=".format(input_str.replace(" ","+"))
+    sample_url = "https://da.gd/s?url=https://www.google.com/search?q={}&btnG=".format(input_str.replace(" ","+"))
     response_api = requests.get(sample_url).text
     if response_api:
         link = response_api.rstrip()
-        await event.edit("Your Search For {} Is Successful\n[Here]({})Is The Link ".format(input_str, link))
+        await event.edit("Your Search For {} Is Successful\n[Here]({}) Is The Link ".format(input_str, link))
     else:
         await event.edit("something is wrong. please try again later.")
