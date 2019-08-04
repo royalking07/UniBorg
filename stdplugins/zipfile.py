921bea8793e4a656a0aa7f16ee39a01070f4f67c
SnapDragon <52431319+SnapDragon7410@users.noreply.github.com>
2019-08-04 19:40:23 +0200

    test1

--- stdplugins/zipfile.py
+++ stdplugins/zipfile.py
@@ -1,25 +1,47 @@
 """Plugin Made By SnapDragon
-Use: .zip <file_directory> <file_name>"""
+Use: .compress"""
 import zipfile
 from telethon import events
+from telethon.tl.types import DocumentAttributeVideo
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("earth ?(.*) ?(.*)"))
+@borg.on(admin_cmd("compress"))
 async def _(event):
-	if event.fwd_from:
-		return
-  file_dir = event.pattern_match.group(1)
-  input_str = event.pattern_match.group(2)
-  if not file_dir is None:
-      if not input_str is None:
-          zip_file = zipfile.ZipFile(file_dir, "w")
-          zip_file.write(input_str + ".zip", compress_type=zipfile.ZIP_DEFLATED)
-          zip_file.close()
-          await event.edit("File Succesfully Zipped To `{}`".format(input_str + ".zip")
-      else:
-          await event.edit("Please Enter A Zip Name")
-  else:
-      await event.edit("Please Enter A File Directory")
-      
-      
+    if event.fwd_from:
+        return
+    if not event.is_reply:
+        await event.edit("Reply to a file to compress it")
+        return
+    directory_name = await event.get_reply_message()
+    if not directory_name is None:
+        zipf = zipfile.ZipFile(directory_name + ".zip", "w", zipfile.ZIP_DEFLATED)
+        zipdir(directory_name, zipf)
+        zipf.close()
+        await borg.send_file(
+            event.chat_id,
+            directory_name + ".zip",
+            caption=file_caption,
+            force_document=True,
+            allow_cache=False,
+            reply_to=event.message.id,
+            progress_callback=progress
+        )
+        try:
+            os.remove(directory_name + ".zip")
+            os.remove(directory_name)
+        except:
+            pass
+        await event.edit("task Completed")
+        await asyncio.sleep(3)
+        await event.delete()
+    else:
+        await event.edit("Invalid Message Type")
+
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+            os.remove(os.path.join(root, file))
