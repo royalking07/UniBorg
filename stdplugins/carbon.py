c697afa1252d0d6f7243db840418b54453cc51dd
SnapDragon <maliktahir0702@gmail.com>
2019-08-11 16:39:25 +0200

    Update carbon.py

--- stdplugins/carbon.py
+++ stdplugins/carbon.py
@@ -63,8 +63,8 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="`---Carbonated Text Created By SnapDragon---`\n\n---Carbonated Text Value---:\n`{}`".format(code.replace("+", " ")),
-         force_document=True,
+         caption="《《CARBON BY SNAPDRAGON》》",
+         force_document=False,
          reply_to=e.message.reply_to_msg_id,
          )
 
