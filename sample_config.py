5de71810f45d541087f33289ab7630e6dccd90d4
SnapDragon <maliktahir0702@gmail.com>
2019-08-12 17:47:33 +0200

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -9,6 +9,8 @@ class Config(object):
     # Get this value from my.telegram.org! Please do not steal
     APP_ID = int(os.environ.get("APP_ID", 6))
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", "/app/.chromedriver/bin/chromedriver")
+    GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome")
     # string session for running on Heroku
     # some people upload their session files on GitHub or other third party hosting
     # websites, this might prevent the un-authorized use of the
