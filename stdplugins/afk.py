e76b8d6aa813f32e503ea82da2568496931284ff
SnapDragon <maliktahir0702@gmail.com>
2019-08-13 16:12:18 +0200

    Update afk.py

--- stdplugins/afk.py
+++ stdplugins/afk.py
@@ -1,4 +1,93 @@
-ason}"
+"""AFK Plugin for @UniBorg
+Syntax: .afk REASON"""
+import asyncio
+import datetime
+from telethon import events
+from telethon.tl import functions, types
+from telethon.utils import resolve_id
+from uniborg.util import admin_cmd
+
+
+borg.storage.USER_AFK = {}  # pylint:disable=E0602
+borg.storage.afk_time = None  # pylint:disable=E0602
+borg.storage.last_afk_message = {}  # pylint:disable=E0602
+borg.storage.recvd_messages = {}  # pylint:disable=E0602
+
+
+@borg.on(events.NewMessage(outgoing=True))  # pylint:disable=E0602
+async def set_not_afk(event):
+    current_message = event.message.message
+    if Config.COMMAND_HAND_LER + "afk" not in current_message and "yes" in borg.storage.USER_AFK:  # pylint:disable=E0602
+        borg.storage.USER_AFK = {}  # pylint:disable=E0602
+        borg.storage.afk_time = None  # pylint:disable=E0602
+        # pylint:disable=E0602
+        for chat_id in borg.storage.last_afk_message:
+            await borg.storage.last_afk_message[chat_id].delete()
+        borg.storage.last_afk_message = {}  # pylint:disable=E0602
+        recvd_messages = "You received the following messages: \n"
+        # pylint:disable=E0602
+        for chat_id in borg.storage.recvd_messages:  # pylint:disable=E0602
+            current_message = borg.storage.recvd_messages[chat_id]
+            user_id = current_message.from_id
+            message_id = current_message.id
+            chat_id, _ = resolve_id(chat_id)
+            if isinstance(_, types.PeerUser):
+                recvd_messages += f"👉 [{chat_id}](tg://user?id={chat_id})"
+                # sadly, there is no way to goto a particular message by a user,
+                # after the 5.5 Android update
+            else:
+                recvd_messages += f"👉 https://t.me/c/{chat_id}/{message_id} \n"
+        try:
+            if recvd_messages != "You received the following messages: \n":
+                await borg.send_message(  # pylint:disable=E0602
+                    Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
+                    recvd_messages,
+                    link_preview=False
+                )
+            await borg.send_message(  # pylint:disable=E0602
+                Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
+                "Set AFK mode to False"
+            )
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await borg.send_message(  # pylint:disable=E0602
+                event.chat_id,
+                "Please set PRIVATE_GROUP_BOT_API_ID " + \
+                "for the proper functioning of afk functionality " + \
+                "in @UniBorg\n\n {}".format(str(e)),
+                reply_to=event.message.id,
+                silent=True
+            )
+        borg.storage.recvd_messages = {}
+
+
+@borg.on(admin_cmd("afk ?((.|\n)*)"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    if Config.PRIVATE_GROUP_BOT_API_ID is None:
+        await event.edit("Please set the required environment variable PRIVATE_GROUP_BOT_API_ID for this plugin to work")
+        return
+    reason = event.pattern_match.group(1)
+    if not borg.storage.USER_AFK:  # pylint:disable=E0602
+        last_seen_status = await borg(  # pylint:disable=E0602
+            functions.account.GetPrivacyRequest(
+                types.InputPrivacyKeyStatusTimestamp()
+            )
+        )
+        # logger.info(last_seen_status)
+        if len(last_seen_status.rules) > 0 and isinstance(last_seen_status.rules[0], types.PrivacyValueAllowAll):
+            borg.storage.afk_time = datetime.datetime.now()  # pylint:disable=E0602
+        borg.storage.USER_AFK.update({"yes": reason})  # pylint:disable=E0602
+        if reason:
+            await event.edit(f"Set AFK mode to True, and Reason is {reason}")
+        else:
+            await event.edit(f"Set AFK mode to True")
+        await asyncio.sleep(5)
+        await event.delete()
+        try:
+            await borg.send_message(  # pylint:disable=E0602
+                Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
+                f"Set AFK mode to True, and Reason is {reason}"
             )
         except Exception as e:  # pylint:disable=C0103,W0703
             logger.warn(str(e))  # pylint:disable=E0602
