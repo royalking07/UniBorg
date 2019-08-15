5a703b1ac0ca6c929cc3de23be335ce269f8416d
SnapDragon <maliktahir0702@gmail.com>
2019-08-15 19:05:11 +0200

    Update slap.py

--- stdplugins/slap.py
+++ stdplugins/slap.py
@@ -6,6 +6,7 @@ usage:- .slap in reply to any message, or u gonna slap urself.
 
 import sys
 from telethon import events, functions
+from telethon.utils import pack_bot_file_id
 import random
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
