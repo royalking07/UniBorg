348cca0cad762f0efc35a794a40be0138efdace7
SnapDragon <maliktahir0702@gmail.com>
2019-08-17 11:13:21 +0200

    Update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -55,7 +55,7 @@ async def _(event):
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
         packname = f"{userid}'s @AnimatedStickersGroup"
-        packshortname = f"SnapDragon_Animated"  # format: Uni_Borg_userid
+        packshortname = f"Uni_Borg_{userid}_as"  # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
@@ -66,7 +66,6 @@ async def _(event):
             uploaded_sticker = await borg.upload_file(sticker, file_name=file_ext_ns_ion)
 
     await event.edit("Processing this sticker. Please Wait!")
-    print(uploaded_sticker)
 
     async with borg.conversation("@Stickers") as bot_conv:
         now = datetime.datetime.now()
@@ -86,12 +85,16 @@ async def _(event):
                 return
             w = await bot_conv.send_file(
                 file=uploaded_sticker,
+                allow_cache=False,
                 force_document=True
             )
-            print(w)
-            await bot_conv.get_response()
+            response = await bot_conv.get_response()
+            if "Sorry" in response.text:
+                await event.edit(f"**FAILED**! @Stickers replied: {response.text}")
+                return
             await silently_send_message(bot_conv, sticker_emoji)
             await silently_send_message(bot_conv, "/publish")
+            response = await silently_send_message(bot_conv, f"<{packname}>")
             await silently_send_message(bot_conv, "/skip")
             response = await silently_send_message(bot_conv, packshortname)
             if response.text == "Sorry, this short name is already taken.":
@@ -103,9 +106,13 @@ async def _(event):
             await silently_send_message(bot_conv, packshortname)
             await bot_conv.send_file(
                 file=uploaded_sticker,
+                allow_cache=False,
                 force_document=True
             )
             response = await bot_conv.get_response()
+            if "Sorry" in response.text:
+                await event.edit(f"**FAILED**! @Stickers replied: {response.text}")
+                return
             await silently_send_message(bot_conv, response)
             await silently_send_message(bot_conv, sticker_emoji)
             await silently_send_message(bot_conv, "/done")
@@ -235,13 +242,16 @@ async def _(event):
 # Helpers
 
 def is_it_animated_sticker(message):
-    if message.media and message.media.document:
-        mime_type = message.media.document.mime_type
-        if "tgsticker" in mime_type:
-            return True
+    try:
+        if message.media and message.media.document:
+            mime_type = message.media.document.mime_type
+            if "tgsticker" in mime_type:
+                return True
+            else:
+                return False
         else:
             return False
-    else:
+    except:
         return False
 
 
