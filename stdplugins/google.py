e98c0f7207bab04e734207f1909a4558dd0592cf
SnapDragon <maliktahir0702@gmail.com>
2019-08-23 17:45:58 +0200

    Update google.py

--- stdplugins/google.py
+++ stdplugins/google.py
@@ -6,47 +6,40 @@ Available Commands:
 
 import asyncio
 import os
+from re import findall
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.options import Options
-from google_images_download import google_images_download
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from gsearch.googlesearch import search
-from uniborg.util import admin_cmd
+from requests import get
 from urllib.parse import quote_plus
 from urllib.error import HTTPError
-import json
-from requests import get
+from google_images_download import google_images_download
+from gsearch.googlesearch import search
+from uniborg.util import admin_cmd
 
 
 def progress(current, total):
     logger.info("Downloaded {} of {}\nCompleted {}".format(current, total, (current / total) * 100))
     
     
-@borg.on(admin_cmd("google search (.*)"))
-async def gsearch(q_event):
-    """ For .google command, do a Google search. """
-    if not q_event.text[0].isalpha() and q_event.text[0] not in (
-            "/", "#", "@", "!"):
-        match_ = q_event.pattern_match.group(1)
-        match = quote_plus(match_)
-        plain_txt = get(f"https://www.startpage.com/do/search?cmd=process_search&query={match}", 'html').text
-        soup = BeautifulSoup(plain_txt, "lxml")
-        
-        msg = ""
-        for result in soup.find_all('a', {'class': 'w-gl__result-title'}):
-            title = result.text
-            link = result.get('href')
-            msg += f"------------------------------------------------------------------------[{title}]({link})------------------------------------------------------------------------\n\n"
-            
-        await q_event.edit(
-            "**Search Query:**\n`" + match_ + "`\n\n**Results:**\n" + msg,
-            link_preview = False
-        )
+@borg.on(admin_cmd("go (.*)"))
+async def _(event):
+    await event.edit("`UniBorg is Getting Information From Google Please Wait... ✍️🙇`")
+    match_ = event.pattern_match.group(1)
+    match = quote_plus(match_)
+    if not match:
+        await event.edit("`I can't search nothing !!`")
+        return
+    plain_txt = get(f"https://www.startpage.com/do/search?cmd=process_search&query={match}", 'html').text
+    soup = BeautifulSoup(plain_txt, "lxml")
+    msg = ""
+    for result in soup.find_all('a', {'class': 'w-gl__result-title'}):
+        title = result.text
+        link = result.get('href')
+        msg += f"**{title}**{link}\n"
+    await event.edit(
+        "**Google Search Query:**\n\n`" + match_ + "`\n\n**Results:**\n" + msg,
+        link_preview = False)
 
 @borg.on(admin_cmd("google image (.*)"))
 async def _(event):
